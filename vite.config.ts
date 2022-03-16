7ddac2b7ad8b15e7d544a04b1e533136cb8d8252
huang.peng <huang.peng@immomo.com>
2022-03-16 20:03:36 +0800

    feat: 🎸 创建工程

---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from "vite";
+import * as fs from "fs";
+import * as path from "path";
+
+// console.log(fs);
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // build: {
+  //   rollupOptions: {
+  //     input: inputFn(),
+  //   },
+  // },
+  plugins: [],
+});
+
+function inputFn(): Record<string, string> {
+  let root = path.join(process.cwd(), "pages");
+  let data = fs.readdirSync(root);
+  let dirMap: Record<string, string> = {};
+  // console.log(path.resolve(__dirname, "index.html"));
+
+  data.forEach((d) => {
+    dirMap[d] = path.join(root, d, "index.html");
+  });
+  console.log(dirMap);
+
+  return dirMap;
+}
