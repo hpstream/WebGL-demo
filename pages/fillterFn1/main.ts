c499b34b8fec0bc022c9960b51535ffc96c3e7e9
huang.peng <huang.peng@immomo.com>
2022-04-13 19:23:24 +0800

    feat: 🎸 完成案例

--- pages/fillterFn1/main.ts
+++ pages/fillterFn1/main.ts
@@ -1,20 +1,144 @@
 const canvas = document.querySelector("#app") as HTMLCanvasElement;
 const context = canvas.getContext("2d");
 import { getImageData, loadImage, traverse } from "./../../src/lib/utils";
+import { brightness, contrast, grayscale, hueRotate, invert, opacity, saturate, sepia, transformColor } from "./../../src/lib/color-matrix";
+
+
+function getImagesData(img, rate) {
+  return getImageData({
+    img: img,
+    rate: rate,
+  });
+}
 
 (async function () {
   const img = await loadImage("/src/assets/dlam.png");
-  const imageData = getImageData(img);
+  let rate = 1 / 3;
+  let imageData = getImagesData(img, rate)
+  // console.log(imageData)
 
-  traverse(imageData, ({ r, g, b, a }) => {
+  //更新; canvas; 内容;
+  canvas.width = imageData.width;
+  canvas.height = imageData.height;
+  context.putImageData(imageData, 0 * rate * canvas.width,
+    0 * rate * canvas.height);
+
+  // 灰度函数
+  let imageData1 = getImagesData(img, rate)
+  traverse(imageData1, ({ r, g, b, a }) => {
     // 对每个像素进行灰度化处理;
     const v = 0.2126 * r + 0.7152 * g + 0.0722 * b;
     return [v, v, v, a];
   });
-  //更新; canvas; 内容;
-  canvas.width = imageData.width;
-  canvas.height = imageData.height;
-  context.putImageData(imageData, 0, 0);
+
+  context.putImageData(
+    imageData1,
+    1 * rate * canvas.width,
+    0 * rate * canvas.height
+  );
+  // 灰度函数
+  let imageData11 = getImagesData(img, rate)
+  traverse(imageData11, ({ r, g, b, a }) => {
+    // 对每个像素进行灰度化处理;
+    // const v = 0.2126 * r + 0.7152 * g + 0.0722 * b;
+    return transformColor([r, g, b, a], grayscale(1));
+
+  });
+  context.putImageData(
+    imageData11,
+    1 * rate * canvas.width,
+    0 * rate * canvas.height
+  );
+  // 将图像转换为深褐色
+  let imageData2 = getImagesData(img, rate)
+  traverse(imageData2, ({ r, g, b, a }) => {
+    // 对每个像素进行灰度化处理;
+    // const v = 0.2126 * r + 0.7152 * g + 0.0722 * b;
+    return transformColor([r, g, b, a], sepia(1));
+
+  });
+  context.putImageData(
+    imageData2,
+    2 * rate * canvas.width,
+    0 * rate * canvas.height
+  );
+
+  // 亮度使用
+  let imageData3 = getImagesData(img, rate)
+  traverse(imageData3, ({ r, g, b, a }) => {
+    // 对每个像素进行灰度化处理;
+    return transformColor([r, g, b, a], brightness(1.4));
+
+  });
+  context.putImageData(
+    imageData3,
+    0 * rate * canvas.width,
+    1 * rate * canvas.height
+  );
+
+  // 饱和度使用
+  let imageData4 = getImagesData(img, rate)
+  traverse(imageData4, ({ r, g, b, a }) => {
+    // 对每个像素进行灰度化处理;
+    return transformColor([r, g, b, a], saturate(5));
+
+  });
+  context.putImageData(
+    imageData4,
+    1 * rate * canvas.width,
+    1 * rate * canvas.height
+  );
+
+  // 对比度使用
+  let imageData5 = getImagesData(img, rate)
+  traverse(imageData5, ({ r, g, b, a }) => {
+    // 对每个像素进行灰度化处理;
+    return transformColor([r, g, b, a], contrast(1.6));
+
+  });
+  context.putImageData(
+    imageData5,
+    2 * rate * canvas.width,
+    1 * rate * canvas.height
+  );
+
+  // 对比度使用
+  let imageData6 = getImagesData(img, rate)
+  traverse(imageData6, ({ r, g, b, a }) => {
+    // 对每个像素进行灰度化处理;
+    return transformColor([r, g, b, a], opacity(0.56));
+
+  });
+  context.putImageData(
+    imageData6,
+    0 * rate * canvas.width,
+    2 * rate * canvas.height
+  );
+
+  // 反色，p=0原色，p=1完全反色
+  let imageData7 = getImagesData(img, rate)
+  traverse(imageData7, ({ r, g, b, a }) => {
+    // 对每个像素进行灰度化处理;
+    return transformColor([r, g, b, a], invert(1));
+
+  });
+  context.putImageData(
+    imageData7,
+    1 * rate * canvas.width,
+    2 * rate * canvas.height
+  );
+  //色相旋转，将色调沿极坐标转过deg角度
+  let imageData8 = getImagesData(img, rate)
+  traverse(imageData8, ({ r, g, b, a }) => {
+    // 对每个像素进行灰度化处理;
+    return transformColor([r, g, b, a], hueRotate(100));
+
+  });
+  context.putImageData(
+    imageData8,
+    2 * rate * canvas.width,
+    2 * rate * canvas.height
+  );
 })();
 
-export {};
+export { };
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,3 @@
-import { inflateRaw } from "zlib";
-
 export function loadImage(src: string) {
   const img = new Image();
   img.crossOrigin = "anonymous";
@@ -14,16 +12,35 @@ export function loadImage(src: string) {
 // 获取图片的 imageData 数据
 const imageDataContext = new WeakMap();
 
-export function getImageData(
-  img: HTMLImageElement,
-  rect: [number, number, number, number] = [0, 0, img.width, img.height]
-) {
+// rate width/height
+export function getImageData({
+  img,
+  rect = [0, 0, img.width, img.height],
+  rate = 1,
+}: {
+  img: HTMLImageElement;
+  rect?: [number, number, number, number];
+  rate?: number;
+}): ImageData {
   let context: OffscreenCanvasRenderingContext2D;
   if (imageDataContext.has(img)) context = imageDataContext.get(img);
   else {
-    const canvas = new OffscreenCanvas(img.width, img.height);
+    var width = img.width;
+    var height = img.height;
+    const canvas = new OffscreenCanvas(width * rate, height * rate);
     context = canvas.getContext("2d") as OffscreenCanvasRenderingContext2D;
-    context.drawImage(img, 0, 0);
+
+    context.drawImage(
+      img,
+      0,
+      0,
+      width,
+      height,
+      0,
+      0,
+      width * rate,
+      height * rate
+    );
     imageDataContext.set(img, context);
   }
   return context.getImageData(...rect);
@@ -57,17 +74,6 @@ export function getPixelXY(imgData: ImageData, x: number, y: number) {
 //   return canvas.transferToImageBitmap();
 // }
 
-// interface params {
-//   r: number;
-//   g: number;
-//   b: number;
-//   a: number;
-//   index: number;
-//   width: number;
-//   height: number;
-//   x: number;
-//   y: number;
-// }
 export function traverse(
   imageData: ImageData,
   pass: (params: {
@@ -103,22 +109,22 @@ export function traverse(
   return imageData;
 }
 
-// function gaussianMatrix(radius, sigma = radius / 3) {
-//   const a = 1 / (Math.sqrt(2 * Math.PI) * sigma);
-//   const b = -1 / (2 * sigma ** 2);
-//   let sum = 0;
-//   const matrix = [];
-//   for (let x = -radius; x <= radius; x++) {
-//     const g = a * Math.exp(b * x ** 2);
-//     matrix.push(g);
-//     sum += g;
-//   }
+function gaussianMatrix(radius, sigma = radius / 3) {
+  const a = 1 / (Math.sqrt(2 * Math.PI) * sigma);
+  const b = -1 / (2 * sigma ** 2);
+  let sum = 0;
+  const matrix = [];
+  for (let x = -radius; x <= radius; x++) {
+    const g = a * Math.exp(b * x ** 2);
+    matrix.push(g);
+    sum += g;
+  }
 
-//   for (let i = 0, len = matrix.length; i < len; i++) {
-//     matrix[i] /= sum;
-//   }
-//   return { matrix, sum };
-// }
+  for (let i = 0, len = matrix.length; i < len; i++) {
+    matrix[i] /= sum;
+  }
+  return { matrix, sum };
+}
 
 /**
  * 高斯模糊
@@ -129,59 +135,59 @@ export function traverse(
  * @param  {Number} sigma 标准方差, 可选, 默认取值为 radius / 3
  * @return {Array}
  */
-// export function gaussianBlur(
-//   pixels,
-//   width,
-//   height,
-//   radius = 3,
-//   sigma = radius / 3
-// ) {
-//   const { matrix, sum } = gaussianMatrix(radius, sigma);
-//   // x 方向一维高斯运算
-//   for (let y = 0; y < height; y++) {
-//     for (let x = 0; x < width; x++) {
-//       let r = 0,
-//         g = 0,
-//         b = 0;
+export function gaussianBlur(
+  pixels,
+  width,
+  height,
+  radius = 3,
+  sigma = radius / 3
+) {
+  const { matrix, sum } = gaussianMatrix(radius, sigma);
+  // x 方向一维高斯运算
+  for (let y = 0; y < height; y++) {
+    for (let x = 0; x < width; x++) {
+      let r = 0,
+        g = 0,
+        b = 0;
 
-//       for (let j = -radius; j <= radius; j++) {
-//         const k = x + j;
-//         if (k >= 0 && k < width) {
-//           const i = (y * width + k) * 4;
-//           r += pixels[i] * matrix[j + radius];
-//           g += pixels[i + 1] * matrix[j + radius];
-//           b += pixels[i + 2] * matrix[j + radius];
-//         }
-//       }
-//       const i = (y * width + x) * 4;
-//       // 除以 sum 是为了消除处于边缘的像素, 高斯运算不足的问题
-//       pixels[i] = r / sum;
-//       pixels[i + 1] = g / sum;
-//       pixels[i + 2] = b / sum;
-//     }
-//   }
+      for (let j = -radius; j <= radius; j++) {
+        const k = x + j;
+        if (k >= 0 && k < width) {
+          const i = (y * width + k) * 4;
+          r += pixels[i] * matrix[j + radius];
+          g += pixels[i + 1] * matrix[j + radius];
+          b += pixels[i + 2] * matrix[j + radius];
+        }
+      }
+      const i = (y * width + x) * 4;
+      // 除以 sum 是为了消除处于边缘的像素, 高斯运算不足的问题
+      pixels[i] = r / sum;
+      pixels[i + 1] = g / sum;
+      pixels[i + 2] = b / sum;
+    }
+  }
 
-//   // y 方向一维高斯运算
-//   for (let x = 0; x < width; x++) {
-//     for (let y = 0; y < height; y++) {
-//       let r = 0,
-//         g = 0,
-//         b = 0;
+  // y 方向一维高斯运算
+  for (let x = 0; x < width; x++) {
+    for (let y = 0; y < height; y++) {
+      let r = 0,
+        g = 0,
+        b = 0;
 
-//       for (let j = -radius; j <= radius; j++) {
-//         const k = y + j;
-//         if (k >= 0 && k < height) {
-//           const i = (k * width + x) * 4;
-//           r += pixels[i] * matrix[j + radius];
-//           g += pixels[i + 1] * matrix[j + radius];
-//           b += pixels[i + 2] * matrix[j + radius];
-//         }
-//       }
-//       const i = (y * width + x) * 4;
-//       pixels[i] = r / sum;
-//       pixels[i + 1] = g / sum;
-//       pixels[i + 2] = b / sum;
-//     }
-//   }
-//   return pixels;
-// }
+      for (let j = -radius; j <= radius; j++) {
+        const k = y + j;
+        if (k >= 0 && k < height) {
+          const i = (k * width + x) * 4;
+          r += pixels[i] * matrix[j + radius];
+          g += pixels[i + 1] * matrix[j + radius];
+          b += pixels[i + 2] * matrix[j + radius];
+        }
+      }
+      const i = (y * width + x) * 4;
+      pixels[i] = r / sum;
+      pixels[i + 1] = g / sum;
+      pixels[i + 2] = b / sum;
+    }
+  }
+  return pixels;
+}
