938c30784cae91bc855f94ddbbd3c622e01d11c9
huang.peng <huang.peng@immomo.com>
2022-10-14 16:42:43 +0800

    feat: 🎸 提交代码

---
+++ pages/代码/5. 颜色与纹理/3.纹理/lib/Two.ts
@@ -0,0 +1,119 @@
+type types = 'POINTS' |
+  'TRIANGLES' |
+  'TRIANGLE_STRIP' |
+  'TRIANGLE_FAN' |
+  'LINES' |
+  'LINE_STRIP' |
+  'LINE_LOOP';
+
+interface attributeParams {
+  size: number;
+  index: number;
+}
+
+interface uniformsParams {
+  type: string;
+  value: any;
+}
+
+
+interface TowParams {
+  gl: WebGLRenderingContext;
+  program: WebGLProgram;
+  type?: types;
+  source: Float32Array | Float64Array;
+  attribute: Record<string, attributeParams>;
+  uniforms: Record<string, uniformsParams>;
+  // sourceSize: number;
+  // elementBytes: number;
+  // categorySize: number;
+}
+
+const defAttr = () => ({
+  gl: null,
+  program: null,
+  type: 'POINTS',
+  source: new Float32Array(),
+  sourceSize: 0,
+  elementBytes: 0,
+  categorySize: 0,
+  categoryBytes: 0,
+  attributes: {},
+  uniforms: {},
+})
+
+export class Two<T extends TowParams, A extends T['attribute'], U extends T['uniforms']> implements TowParams {
+  gl: WebGLRenderingContext;
+  type?: types;
+  source: Float32Array | Float64Array;
+  attribute: Record<keyof A, attributeParams>;
+  uniforms: Record<keyof U, uniformsParams>;
+  sourceSize: number; // 顶点
+  elementBytes: number; // 
+  categorySize: number;//一个类目的数据
+  categoryBytes: number;
+  program: WebGLProgram;
+
+  constructor(attr: T) {
+    Object.assign(this, defAttr(), attr);
+    this.init();
+  }
+
+  init() {
+    this.calculateSourceSize();
+    this.initAttribute();
+    this.initUniforms();
+  }
+  calculateSourceSize() {
+    this.elementBytes = this.source.BYTES_PER_ELEMENT;
+    let { attribute, source } = this;
+    let categorySize = 0;
+
+    Object.entries(attribute).forEach(([key, value]) => {
+      categorySize += value.size;
+    })
+    this.categorySize = categorySize;
+    this.categoryBytes = categorySize * this.elementBytes;
+    this.sourceSize = source.length / categorySize;
+
+
+  }
+  initAttribute() {
+    let { attribute, gl, program, source, categorySize, categoryBytes, elementBytes } = this;
+
+    // 缓冲对象
+    const vertexBuffer = gl.createBuffer();
+    // 绑定缓冲对象
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    // 写入数据
+    gl.bufferData(gl.ARRAY_BUFFER, source, gl.STATIC_DRAW);
+
+    Object.entries(attribute).forEach(([key, value]) => {
+      console.log(value.size, categoryBytes, value.index * elementBytes)
+      let attr = gl.getAttribLocation(program, key);
+      gl.vertexAttribPointer(attr, value.size, gl.FLOAT, false, categoryBytes, value.index * elementBytes);
+      gl.enableVertexAttribArray(attr);
+    })
+
+  }
+  initUniforms() {
+    let { gl, program, uniforms } = this;
+    Object.entries(uniforms).forEach(([key, val]) => {
+      let uniform = gl.getUniformLocation(program, key);
+      const { type, value } = val;
+      if (type.includes('Matrix')) {
+        gl[type](uniform, false, value);
+      } else {
+        gl[type](uniform, value)
+      }
+
+
+    })
+
+
+  }
+  render(type = this.type) {
+    const { gl, sourceSize } = this;
+    gl.drawArrays(gl[type], 0, sourceSize)
+  }
+}
---
+++ pages/代码/5. 颜色与纹理/5.案例1-魔方/main.ts
@@ -0,0 +1,113 @@
+// 导入着色器shader, '?rwa' vite支持的语法，认识是字符串
+import vertexShader from "./glsl/vertexShader.glsl?raw";
+
+import fragmentShader from "./glsl/fragmentShader.glsl?raw";
+
+
+import * as dat from "dat.gui";
+
+import { initShaders } from "./lib/utils";
+import { Matrix3, Matrix4 } from "three";
+import { Two } from "./lib/Two";
+
+// 1.获取canvas节点
+const canvas = document.querySelector('#canvas') as HTMLCanvasElement;
+
+// 2.设置宽高
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+// 3.获取三维画笔
+const gl = canvas.getContext('webgl') as WebGLRenderingContext;
+
+// 封装着色器程序
+const program = initShaders(gl, vertexShader, fragmentShader);
+
+
+
+const maxV = 1;
+const maxU = 1;
+const source = new Float32Array([
+  -0.5, 0.5, 0, maxV,
+  -0.5, -0.5, 0, 0.0,
+  0.5, 0.5, maxU, maxV,
+  0.5, -0.5, maxU, 0.0,
+]);
+let two = new Two({
+  gl,
+  program,
+  source: source,
+  type: 'TRIANGLE_STRIP',
+  attribute: {
+    'a_Position': {
+      size: 2,
+      index: 0
+    },
+    'a_Pin': {
+      size: 2,
+      index: 2
+    }
+
+  },
+  uniforms: {},
+})
+
+
+let img = new Image();
+img.src = './img/600.png';
+
+img.onload = () => {
+  /* 图像预处理 */
+  gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1);
+  /* 准备三个角色 */
+  gl.activeTexture(gl.TEXTURE0);
+  const texture = gl.createTexture();
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+  gl.texImage2D(
+    gl.TEXTURE_2D,
+    0,
+    gl.RGB,
+    gl.RGB,
+    gl.UNSIGNED_BYTE,
+    img
+  )
+
+  gl.texParameteri(
+    gl.TEXTURE_2D,
+    gl.TEXTURE_MIN_FILTER,
+    gl.LINEAR
+  )
+  gl.texParameteri(
+    gl.TEXTURE_2D,
+    gl.TEXTURE_WRAP_S,
+    gl.CLAMP_TO_EDGE
+  )
+  gl.texParameteri(
+    gl.TEXTURE_2D,
+    gl.TEXTURE_WRAP_T,
+    gl.CLAMP_TO_EDGE
+  )
+  // gl.texParameteri(
+  //   gl.TEXTURE_2D,
+  //   gl.TEXTURE_WRAP_S,
+  //   gl.MIRRORED_REPEAT
+  // )
+  // 非二次幂图像展示
+  // 镜像复制
+  // 复制
+
+
+  const u_Sampler = gl.getUniformLocation(program, 'u_Sampler')
+  gl.uniform1i(u_Sampler, 0)
+  gl.clearColor(0, 0, 0, 1)
+  // 5.刷底色
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  two.render();
+}
+
+
+
+
+
+
